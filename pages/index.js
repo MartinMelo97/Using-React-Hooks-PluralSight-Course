924c202821df14d2ce7f87d936e9c9e61e782491
Martín Melo <martin.melo.dev.97@gmail.com>
2021-01-04 08:46:25 -0600

    Finishing setup boilerplate

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React from "react";
+import App from "../src/App";
 
-const InputElement = () => {
-};
+function index() {
+  return <App pageName="Home" />;
+}
 
-export default InputElement;
+export default index;
