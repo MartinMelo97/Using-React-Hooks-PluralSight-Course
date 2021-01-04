4d750e4f9a3e6c48ea59a9cbeae651797bba7448
Martín Melo <martin.melo.dev.97@gmail.com>
2021-01-04 07:59:08 -0600

    Removed files to start with the new module

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,6 @@
 import React, { useState } from 'react';
 
 const InputElement = () => {
-
-  const [inputText, setInputText] = useState('');
-  const [historyList, setHistoryList] = useState([]);
-
-  const handleInputText = (event) => {
-    setInputText(event.target.value);
-    addItemToHistoryList(event);
-  };
-
-  const addItemToHistoryList = (event) => {
-    setHistoryList([...historyList, event.target.value]);
-  };
-
-  return (
-    <div>
-      <input
-        onChange={handleInputText}
-        placeholder="Enter some text"
-      />
-      <br />
-      <p>Current value: {inputText}</p>
-      <hr />
-      {historyList.map(historyItem => (
-        <p>{historyItem}</p>
-      ))}
-    </div>
-  );
 };
 
 export default InputElement;
